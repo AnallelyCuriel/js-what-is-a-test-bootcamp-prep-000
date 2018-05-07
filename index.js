1977c2b0e8dfc5080275ed09e4ec0436279b526c
Anallely Curiel <anallely.curiel@gmail.com>
2018-05-07 16:43:49 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 var name = "Joe"
 var height = 74
 var message = `${name} is ${height} inches tall`
-var tes= name + "valor" + height +"peso "
+var tes= name + " name " + height +" peso "
 console.log(message)
 console.log(tes)
 
